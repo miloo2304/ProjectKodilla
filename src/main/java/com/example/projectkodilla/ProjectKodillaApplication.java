e80bf88f5371a1dab5a967a2767e144a861fc469
Milosz Podsiadly <m.podsiadly99@gmail.com>
2025-02-06 20:26:57 +0100

    Init

---
+++ src/main/java/com/example/projectkodilla/ProjectKodillaApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectkodilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectKodillaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectKodillaApplication.class, args);
+    }
+
+}
