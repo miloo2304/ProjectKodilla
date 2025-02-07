8fe0163eda8ef12e3d9f32933a53f49dd3a9084f
Miłosz Podsiadły <m.podsiadly99@gmail.com>
2025-02-07 10:42:23 +0100

    6.2 Added kodilla-testing module

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/Project_6_2/SimpleUser.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing.Project_6_2;
+
+public class SimpleUser {
+    private String username;
+
+    public SimpleUser(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
