753915072a347eff38520264ad8b60788730d98e
Miłosz Podsiadły <m.podsiadly99@gmail.com>
2025-03-13 14:19:18 +0100

    12.1 Added kodilla-good-patterns

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Factorial.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Factorial.java
@@ -2,42 +2,40 @@ package com.kodilla.good.patterns.challenges;
 import java.util.Scanner;
 import java.util.InputMismatchException;
 
-
 public class Factorial {
     public int n;
     public int factorial = 1;
     boolean validInput = false;
+    public int result;
     Scanner scanner = new Scanner(System.in);
 
     public int calculateFactorial(int n) {
         if (n < 0) {
             throw new IllegalArgumentException("Factorial isn't defined for negative number");
-        }
-        if (n == 0 || n == 1) {
+        } if (n == 0 || n == 1) {
             return 1;
-        }
-        for (int i = 2; i <= n; i++) {
+        } for (int i = 2; i <= n; i++) {
             factorial *= i;
-        }
-        return factorial;
+        } return factorial;
     }
 
-    public void handleException() {
+    public int handleException() {
         while (!validInput) {
             System.out.print("Please type an integer number: ");
             try {
                 n = scanner.nextInt();
-                int result = calculateFactorial(n);
-                System.out.println("Factorial from " + n + " is: " + result);
+                result = calculateFactorial(n);
                 validInput = true;
             } catch (IllegalArgumentException e) {
                 System.out.println("Error: " + e.getMessage());
+                scanner.nextLine();
             } catch (InputMismatchException e) {
                 System.out.println("You typed incorrect type of data, required integer.");
                 scanner.nextLine();
             }
         }
         scanner.close();
+        return result;
     }
 
     public static void main(String[] args) {
